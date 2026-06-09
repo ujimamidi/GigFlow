30e679937a0e3e3dbb83b11ae3973ebe79c329db
Ujwal Mamidi <uji.mamidi@gmail.com>
2026-06-09 23:09:40 +1000

    lifting gigs state up to parent (landing)

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,16 +2,20 @@ import GigForm from "../components/Gigs/Form/GigForm";
 import GigList from "../components/Gigs/List/GigList";
 import GigStats from "../components/Gigs/Stats/GigStats";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 function Landing() {
+
+    const [gigs, setGigs] = useState([]);
+
     return (
         <div>
             <Navbar />
             <div className="flex flex-row items-start ml-8 mt-8">
-                <GigForm />
+                <GigForm setGigs={setGigs}/>
                 <div className="flex flex-col">
-                    <GigStats />
-                    <GigList />
+                    <GigStats gigs={gigs}/>
+                    <GigList gigs={gigs}/>
                 </div>
             </div>
         </div>
