d381eadcfaf4f1db6e59042fcb07087d67119ef6
Ujwal Mamidi <uji.mamidi@gmail.com>
2026-06-17 00:45:47 +1000

    created basic backend template

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.get("/api", (req, res) => {
+  res.json({message: "Hello from server"});
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
